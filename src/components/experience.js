3e2428c5f99053a167ef1cd215b5a09965bc3357
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2023-04-17 18:45:58 +0200

    Fix experience

--- src/components/experience.js
+++ src/components/experience.js
@@ -1,85 +1,85 @@
 function Experience() {
-  return (
-    <div>
-      <h3>Mi trayectoria</h3>
-      <ul>
-        <li>
-          Mi primer trabajo fue en{" "}
-          <a
-            href="https://www.linkedin.com/company/inmo.design/about/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Inmo.Design
-          </a>
-          , una empresa pequeñita donde empecé a trabajar como programador
-          junior. La empresa se dedica a hacer páginas web para inmobiliarias.
-        </li>
+	return (
+		<div>
+			<h3>Mi trayectoria</h3>
+			<ul>
+				<li>
+					Mi primer trabajo fue en{" "}
+					<a
+						href="https://www.linkedin.com/company/inmo.design/about/"
+						target="_blank"
+						rel="noreferrer">
+						Inmo.Design
+					</a>
+					, una empresa pequeñita donde empecé a trabajar como programador
+					junior. La empresa se dedica a hacer páginas web para inmobiliarias.
+				</li>
 
-        <li>
-          Después hice un Master de Diseño y Desarrollo de Videojuegos en la{" "}
-          <a href="https://evadformacion.com/" target="_blank" rel="noreferrer">
-            EVAD
-          </a>
-          . Esta formación me mostró lo que implica crear un videojuego y me
-          enseñó a ser <strong>autodidacta</strong>.
-        </li>
-        <li>
-          En el trabajo final de master participé en{" "}
-          <a
-            href="https://www.playstationtalents.es/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Playstation Talents
-          </a>{" "}
-          y monté un equipo de videojuegos indie. Aquí aprendí a{" "}
-          <strong>trabajar en equipo</strong> y{" "}
-          <strong>leer mucha documentación</strong>.
-        </li>
-        <li>
-          Las prácticas del Master las hice en{" "}
-          <a
-            href="https://www.linkedin.com/company/bambo-studio/about/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Bambo Studio
-          </a>
-          , donde me enseñaron a hacer juegos móviles.
-        </li>
-        <li>
-          Mi último trabajo fue en{" "}
-          <a
-            href="https://www.linkedin.com/company/isabellatech"
-            target="_blank"
-            rel="noreferrer"
-          >
-            IsabellaTech
-          </a>{" "}
-          donde trabajé como programador Frontend desarrollando proyectos de
-          todo tipo durante los 2 años. Aprendí a{" "}
-          <strong>
-            optimizar el código, escalar un proyecto, predecir tiempos y
-            planificar tareas
-          </strong>
-          .
-        </li>
-        <li>
-          Mi último trabajo fue en{" "}
-          <a
-            href="https://www.https://www.iflastmile.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            if Lastmile
-          </a>{" "}
-          aquí trabajé también como programador Frontend ayudando a desarrollar su aplicación interna y la 
-          aplicación para sus clientes, <strong>los mentores me ayudaron muchísimo en mejorar mis habilidades 
-          de frontend</strong>, también aprendí habilidades de fullstack {"(Amazon Web Services (AWS), Node.js.)"}
-        </li>
-      </ul>
-    </div>
-  );
+				<li>
+					Después hice un Master de Diseño y Desarrollo de Videojuegos en la{" "}
+					<a href="https://evadformacion.com/" target="_blank" rel="noreferrer">
+						EVAD
+					</a>
+					. Esta formación me mostró lo que implica crear un videojuego y me
+					enseñó a ser <strong>autodidacta</strong>.
+				</li>
+				<li>
+					En el trabajo final de master participé en{" "}
+					<a
+						href="https://www.playstationtalents.es/"
+						target="_blank"
+						rel="noreferrer">
+						Playstation Talents
+					</a>{" "}
+					y monté un equipo de videojuegos indie. Aquí aprendí a{" "}
+					<strong>trabajar en equipo</strong> y{" "}
+					<strong>leer mucha documentación</strong>.
+				</li>
+				<li>
+					Las prácticas del Master las hice en{" "}
+					<a
+						href="https://www.linkedin.com/company/bambo-studio/about/"
+						target="_blank"
+						rel="noreferrer">
+						Bambo Studio
+					</a>
+					, donde me enseñaron a hacer juegos móviles.
+				</li>
+				<li>
+					En{" "}
+					<a
+						href="https://www.linkedin.com/company/isabellatech"
+						target="_blank"
+						rel="noreferrer">
+						IsabellaTech
+					</a>{" "}
+					trabajé como programador Frontend desarrollando proyectos de todo tipo
+					durante los 2 años. Aprendí a{" "}
+					<strong>
+						optimizar el código, escalar un proyecto, predecir tiempos y
+						planificar tareas
+					</strong>
+					.
+				</li>
+				<li>
+					Mi último trabajo fue en{" "}
+					<a
+						href="https://www.https://www.iflastmile.com/"
+						target="_blank"
+						rel="noreferrer">
+						if Lastmile
+					</a>{" "}
+					aquí trabajé también como programador Frontend ayudando a desarrollar
+					su aplicación interna y la aplicación para sus clientes,{" "}
+					<strong>
+						los mentores me ayudaron muchísimo en mejorar mis habilidades de
+						frontend
+					</strong>
+					, también aprendí habilidades de fullstack{" "}
+					{"(Amazon Web Services (AWS), Node.js.)"}
+				</li>
+			</ul>
+		</div>
+	);
 }
 export default Experience;
