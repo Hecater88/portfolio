efad60cab481aa59aecc0f2145007f96afe77ba0
Hecater88 <47868066+Hecater88@users.noreply.github.com>
2023-04-17 19:02:52 +0200

    Update skills

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,76 +1,96 @@
 function Skills() {
-  return (
-    <div>
-      <h3>Lo que sé</h3>
-      <ul>
-        <li>
-          Se desarrollar páginas web, temas y plugins de{" "}
-          <a href="https://wordpress.com/es/" target="_blank" rel="noreferrer">
-            WordPress
-          </a>{" "}
-          con PHP.
-        </li>
-        <li>
-          Como desarrollador <strong>frontend</strong> llevo cuatro años
-          programando con <strong>React.js</strong>.
-        </li>
-        <li>
-       Sé hacer test unitarios con {" "}
-          <a href="https://jestjs.io/" target="_blank" rel="noreferrer">
-            Jest
-          </a>
-        </li>
-        <li>
-          Para control de versiones uso <strong>Git</strong> con{" "}
-          <a href="https://github.com/" target="_blank" rel="noreferrer">
-            GitHub
-          </a>
-          .
-        </li>
-        <li>
-          Uso{" "}
-          <a href="https://sass-lang.com/" target="_blank" rel="noreferrer">
-            Sass
-          </a>{" "}
-          para las hojas de estilo. Suelo trabajar sin frameworks aunque
-          actualmente estoy explorando la nueva versión de{" "}
-          <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
-            Tailwind
-          </a>
-          .
-        </li>
-        <li>
-          Estoy familiarizado con el trabajo mediante{" "}
-          <strong>APIs con la interfaz RESTful</strong>.
-        </li>
-        <li>
-          He trabajado con algunos equipos y normalmente con{" "}
-          <strong>Scrum</strong>, pero puedo adaptarme a cualquier proceso de
-          trabajo.
-        </li>
-        <li>
-         Tengo experiencia con {" "}
-          <a href="https://frontity.org//" target="_blank" rel="noreferrer">
-            Frontity
-          </a>
-          , un framework hecho en React especialmente diseñado para WordPress.
-        </li>
-        <li>
-         También estoy muy familiarizado con{" "}
-          <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-            Next.js
-          </a>
-          , un framework de React que te ayuda a hacer aplicaciones muy rápidas. 
-          Puede mostrar páginas del lado del servidor o generarlas estáticamente.
-        </li>
-        <li>
-          Tengo aptitudes para diseñar y desarrollar videojuegos, pero
-          voy a enfocar este portfolio en mi otra rama favorita de la
-          programación que es el Frontend.
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h3>Lo que sé</h3>
+			<ul>
+				<li>
+					Se desarrollar páginas web, temas y plugins de{" "}
+					<a href="https://wordpress.com/es/" target="_blank" rel="noreferrer">
+						WordPress
+					</a>{" "}
+					con PHP.
+				</li>
+				<li>
+					Tengo experiencia con {" "}
+					<a href="https://frontity.org//" target="_blank" rel="noreferrer">
+						Frontity
+					</a>
+					, un framework hecho en React especialmente diseñado para WordPress.
+				</li>
+				<li>
+					Como desarrollador <strong>frontend</strong> llevo cuatro años
+					programando con{" "}
+					<a href="https://es.reactjs.org/" target="_blank" rel="noreferrer">
+						<strong>React.js</strong>
+					</a>
+					. Siempre intento estar al tanto de los frameworks que salen sobre
+					esta librería.
+				</li>
+				<li>
+					También estoy muy familiarizado con{" "}
+					<a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+						Next.js
+					</a>
+					, un framework de React que te ayuda a hacer aplicaciones muy rápidas.
+					Puede mostrar páginas del lado del servidor o generarlas
+					estáticamente.
+				</li>
+				<li>
+					Sé hacer test unitarios con{" "}
+					<a href="https://jestjs.io/" target="_blank" rel="noreferrer">
+						Jest
+					</a>
+					.
+				</li>
+				<li>
+					Para control de versiones uso <strong>Git</strong> con{" "}
+					<a href="https://github.com/" target="_blank" rel="noreferrer">
+						GitHub
+					</a>
+					.
+				</li>
+				<li>
+					Uso{" "}
+					<a href="https://sass-lang.com/" target="_blank" rel="noreferrer">
+						Sass
+					</a>{" "}
+					para las hojas de estilo. Actualmente estoy explorando la nueva
+					versión de{" "}
+					<a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+						Tailwind
+					</a>
+					.
+				</li>
+				<li>
+					Suelo utilizar <strong>librerías de UI Components</strong> para
+					ahorrar tiempo en desarrollar aplicaciones. Estoy muy familiarizado
+					con{" "}
+					<a href="https://mui.com/" target="_blank" rel="noreferrer">
+						MUI
+					</a>{" "}
+					y actualmente investigando
+					<a href="https://chakra-ui.com/" target="_blank" rel="noreferrer">
+						Chakra UI
+					</a>
+					.
+				</li>
+				<li>
+					Estoy familiarizado con el trabajo mediante{" "}
+					<strong>APIs con la interfaz RESTful</strong>.
+				</li>
+				<li>
+					He trabajado con algunos equipos y normalmente con{" "}
+					<strong>Scrum</strong>, pero puedo adaptarme a cualquier proceso de
+					trabajo.
+				</li>
+				<li>
+					Tengo aptitudes para diseñar y desarrollar videojuegos, pero voy a
+					enfocar este portfolio en mi otra rama favorita de la programación que
+					es el Frontend.
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default Skills;
